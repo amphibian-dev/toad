3344025f9113e0d46b3610f5eddaa4218739079e
Secbone <secbone@gmail.com>
2018-11-27 14:55:41 +0800

    update: tox deps

--- setup.py
+++ setup.py
@@ -22,9 +22,6 @@ setup(
         'sklearn',
         'statsmodels',
     ],
-    tests_require = [
-        'graphviz',
-    ],
     license = 'MIT',
     classifiers = [
         'Operating System :: POSIX',
