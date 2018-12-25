ee3c4cba77f85858b7f095950760f895171a6b51
Secbone <secbone@gmail.com>
2018-12-25 15:23:19 +0800

    update: badrate_plot support counts

--- toad/plot.py
+++ toad/plot.py
@@ -4,14 +4,18 @@ import pandas as pd
 import seaborn as sns
 from matplotlib.font_manager import FontProperties
 
+from .utils import unpack_tuple
+
 FONTS_PATH = os.path.join(os.path.abspath(__file__), '../../fonts/pingfang.ttf')
 myfont = FontProperties(fname = os.path.abspath(FONTS_PATH))
 sns.set(font = myfont.get_family())
 
 
-def badrate_plot(frame, x = None, target = 'target', by = None, freq = None, format = None):
+def badrate_plot(frame, x = None, target = 'target', by = None,
+                freq = None, format = None, return_counts = False):
     frame = frame.copy()
 
+    grouper = x
     if freq is not None:
         frame.loc[:, x] = pd.to_datetime(frame[x], format = format)
         grouper = pd.Grouper(key = x, freq = freq)
@@ -23,11 +27,27 @@ def badrate_plot(frame, x = None, target = 'target', by = None, freq = None, for
     table = group[target].agg(['sum', 'count']).reset_index()
     table['badrate'] = table['sum'] / table['count']
 
-    return sns.relplot(
+    rate_plot = sns.relplot(
         x = x,
         y = 'badrate',
         hue = by,
         markers = True,
         kind = 'line',
         data = table,
+        aspect = 2,
     )
+
+    res = (rate_plot,)
+
+    if return_counts:
+        count_plot = sns.catplot(
+            x = x,
+            y = 'count',
+            kind = 'bar',
+            hue = by,
+            data = table,
+            aspect = 2,
+        )
+        res += (count_plot,)
+
+    return unpack_tuple(res)
