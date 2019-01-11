316e5bf9a56977a60c2c552f6f5bd700c5105bb4
Secbone <secbone@gmail.com>
2019-01-11 14:46:18 +0800

    add: corr plot

--- toad/plot.py
+++ toad/plot.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 
 from .utils import unpack_tuple
@@ -11,6 +12,8 @@ sns.set_palette('muted')
 FONTS_PATH = os.path.join(os.path.abspath(__file__), '../../fonts/pingfang.ttf')
 myfont = FontProperties(fname = os.path.abspath(FONTS_PATH))
 sns.set(font = myfont.get_family())
+
+HEATMAP_CMAP = sns.diverging_palette(240, 10, as_cmap = True)
 
 
 def badrate_plot(frame, x = None, target = 'target', by = None,
@@ -72,3 +75,34 @@ def badrate_plot(frame, x = None, target = 'target', by = None,
         res += (table,)
 
     return unpack_tuple(res)
+
+
+def corr_plot(frame):
+    """plot for correlation
+
+    Args:
+        frame (DataFrame): frame to draw plot
+    """
+    corr = frame.corr()
+
+    mask = np.zeros_like(corr, dtype = np.bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    _, ax = plt.subplots(figsize = (20, 15))
+
+    sns.heatmap(
+        corr,
+        mask = mask,
+        cmap = HEATMAP_CMAP,
+        vmax = 1,
+        vmin = -1,
+        center = 0,
+        square = True,
+        cbar_kws = {"shrink": .5},
+        linewidths = .5,
+        annot = True,
+        fmt = '.2f',
+        ax = ax,
+    )
+
+    return ax
