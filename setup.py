9c668409536fe4edd927c9457ef69d2e6914013e
Secbone <secbone@gmail.com>
2019-02-25 16:39:14 +0800

    update: version to 0.0.32

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name = 'toad',
-    version = '0.0.31',
+    version = '0.0.32',
     description = 'python utils for detect data',
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
