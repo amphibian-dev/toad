ac6d45c29ede29c1d9d6a8f168c43bc893745931
Secbone <secbone@gmail.com>
2019-09-23 23:04:38 +0800

    chore: add development version

--- toad/version.py
+++ toad/version.py
@@ -1,2 +1,17 @@
-__version_info__ = (0, 0, 55)
-__version__ = '.'.join(str(x) for x in __version_info__)
+__version_info__ = (0, 0, 55, 'final', 0)
+
+def get_version(version):
+    main = '.'.join(str(x) for x in version[:3])
+
+    if version[3] == 'final':
+        return main
+
+    symbol = {
+        'alpha': 'a',
+        'beta': 'b',
+        'rc': 'rc',
+    }
+
+    return main + symbol[version[3]] + str(version[4])
+
+__version__ = get_version(__version_info__)
