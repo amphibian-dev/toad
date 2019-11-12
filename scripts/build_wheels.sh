d099584ee9c24a0ac8b8d6adf9bb1b56598f3d1c
Secbone <secbone@gmail.com>
2019-11-12 11:51:00 +0800

    update: support linux py38

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -3,7 +3,7 @@ set -e -x
 
 
 # Compile wheels
-for PYBIN in /opt/python/cp3[567]*/bin; do
+for PYBIN in /opt/python/cp3[5678]*/bin; do
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ -w /dist/
 done
