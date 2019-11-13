35ca042645a6e9d560c35d2b631ff6e2b994dfb8
Secbone <secbone@gmail.com>
2019-11-13 17:08:19 +0800

    fix: no deps

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -5,7 +5,7 @@ set -e -x
 # Compile wheels
 for PYBIN in /opt/python/cp3[5678]*/bin; do
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
-    "${PYBIN}/pip" --no-deps wheel /io/ -w /dist/
+    "${PYBIN}/pip" wheel --no-deps /io/ -w /dist/
 done
 
 # Bundle external shared libraries into the wheels
